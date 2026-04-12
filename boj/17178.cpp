eecf0bf894d963a9f58b071e19001190f83a06c8
Friox <devfriox@gmail.com>
2026-04-13 00:10:57 +0900

    solve(boj): 줄서기(G5)

--- boj/17178.cpp
+++ boj/17178.cpp
@@ -1,49 +1,36 @@
 #include <cstdio>
-#include <string>
 #include <vector>
 #include <queue>
 #include <stack>
-std::string cvt(int n) {
-    return std::string(1, n / 10000) + "-" + std::to_string(n % 10000);
-}
+#include <algorithm>
 int main() {
     int N, idx = 0;
     std::queue<int> pool;
     std::stack<int> waiting;
     std::vector<int> check;
     scanf("%d", &N);
-    int cnt = N * 5;
-    for (int i = 0; i < cnt; i++) {
+    for (int i = 0; i < N * 5; i++) {
         char a;
         int b;
         scanf(" %c-%d", &a, &b);
         int n = a * 10000 + b;
-        // printf("%d\n", n);
         pool.push(n);
         check.push_back(n);
     }
-    while (idx < cnt) {
+    std::sort(check.begin(), check.end());
+    while (idx < N * 5) {
         if (!waiting.empty() && waiting.top() == check[idx]) {
-            printf("대기열에서 %s 발견\n", cvt(waiting.top()).c_str());
             waiting.pop();
             idx++;
-            continue;
-        }
-        if (!pool.empty() && pool.front() == check[idx]) {
-            printf("풀에서 %s 발견\n", cvt(pool.front()).c_str());
+        } else if (!pool.empty() && pool.front() == check[idx]) {
             pool.pop();
             idx++;
-            continue;
-        }
-        if (!pool.empty()) {
+        } else if (!pool.empty()) {
             while (!pool.empty() && pool.front() != check[idx]) {
-                printf("%s 를 대기열로 이동\n", cvt(pool.front()).c_str());
                 waiting.push(pool.front());
                 pool.pop();
             }
-            continue;
-        }
-        break;
+        } else break;
     }
-    printf("%s", check.empty() ? "GOOD" : "BAD");
+    printf("%s", idx == N * 5 ? "GOOD" : "BAD");
 }
